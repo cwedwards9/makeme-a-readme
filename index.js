6eee2a7122088bf939644d95ec25bc8af0ef109c
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-20 13:03:37 -0400

    Add question prompts to generate sections in a new README file from the user inputs.

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
 const inquirer = require("inquirer");
 
+// maybe include a default value if nothing is entered
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter your github username: ",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Enter your email address: ",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the title of your README?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Give this readme a description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Include installation instructions for how users can install your app: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Include usage information for how users can use it: ",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "Include the appropriate license for your repo: ",
+        name: "license",
+        choices: ["Apache 2.0", "GNU AGPL v3", "IBM Public License Version 1.0", "The MIT License", "Mozilla Public License 2.0"]
+    },
+    {
+        type: "input",
+        message: "Include contributing guidelines for other interested developers: ",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Include test instructions: ",
+        name: "test"
+    }
+])
+.then(function(response) {
+    console.log(response);
+});
+
+//     if (response.confirm === response.password) {
+//       console.log("Success!");
+//     }
+//     else {
+//       console.log("You forgot your password already?!");
+//     }
+//   });
