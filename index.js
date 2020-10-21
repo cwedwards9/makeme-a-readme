c89704575e17204772443be2eb22e23deda69f64
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-21 11:15:28 -0400

    Add a function to generate a table of contents in markdown format

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const prompts = require("./promptsList");
 
-// maybe include a default value if nothing is entered
+
 inquirer.prompt(prompts)
 .then(function(res) {
     const title = generateTitle(res);
-    const answers = generateContent(res);
-    const markdown = title + '\n' + answers;
+    const tableOfContents = generateTableOfContents();
+    const answers = generateSections(res);
+    
+    const markdown = title + '\n' + tableOfContents + '\n' + answers;
 
     fs.appendFile("README2.md", markdown, (err) => {
         if(err) throw err;
@@ -16,13 +18,25 @@ inquirer.prompt(prompts)
     console.log(res);
 });
 
+
 function generateTitle(data){
     return `# ${data.title} \n`;
 }
 
-function generateContent(data) {
+function generateTableOfContents(){
+    const toc = "## Table of Contents \n" + 
+                "* [Installation](#Installation) \n" +
+                "* [Usage](#Usage) \n" +
+                "* [License](#License) \n" +
+                "* [Contributions](#Contributions) \n" +
+                "* [Tests](#Tests) \n" +
+                "* [Questions](#Questions) \n";
+    return toc;
+}
+
+function generateSections(data) {
     const { github, email } = data;
-    const questionsHeader = "## Questions" + "\n";
+    const questionsHeader = `## Questions \n`;
     const userInfo = "* Username: " + github + "\n" + "* Email: " + email;
 
     return questionsHeader + userInfo;
