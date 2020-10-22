9c70fb43a58d5ffbb011f5c4f8077b3c81a3a7bc
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-22 11:45:12 -0400

    Refactor 'generateTableOfContents' and 'generateSections' functions. Add description to the data being returned from generateTitle function

--- index.js
+++ index.js
@@ -4,47 +4,49 @@ const prompts = require("./promptsList");
 
 
 inquirer.prompt(prompts)
-.then(function(res) {
-    const title = generateTitle(res);
-    const tableOfContents = generateTableOfContents();
-    const sections = generateSections(res);
-    
-    const markdown = title + '\n' + tableOfContents + '\n' + sections;
+    .then(function(res) {
+        const title = generateTitle(res);
+        const tableOfContents = generateTableOfContents();
+        const sections = generateSections(res);
+        
+        const markdown = `${title} \n${tableOfContents} \n${sections}`;
 
-    fs.appendFile("README2.md", markdown, (err) => {
-        if(err) throw err;
-        console.log("Your README file was created successfully!")
-    })
-    console.log(res);
-});
+        fs.appendFile("README2.md", markdown, (err) => {
+            if(err) throw err;
+            console.log("Your README file was created successfully!")
+        })
+        console.log(res);
+    });
 
 
 function generateTitle(data){
-    return `# ${data.title} \n`;
+    const { title, description } = data;
+    return `# ${title} \n${description} \n`;
 }
 
 function generateTableOfContents(){
-    const toc = "## Table of Contents \n" + 
-                "* [Installation](#Installation) \n" +
-                "* [Usage](#Usage) \n" +
-                "* [License](#License) \n" +
-                "* [Contributions](#Contributions) \n" +
-                "* [Tests](#Tests) \n" +
-                "* [Questions](#Questions) \n";
+    const toc = [
+        "## Table of Contents",
+        "* [Installation](#Installation)",
+        "* [Usage](#Usage)",
+        "* [License](#License)",
+        "* [Contributing](#Contributing)",
+        "* [Tests](#Tests)",
+        "* [Questions](#Questions) \n"
+    ].join("\n");
     return toc;
 }
 
 function generateSections(data) {
     const { installation, usage, license, contributing, tests, github, email } = data;
 
-    const installSect = `## Installation \n${installation} \n`;
-    const useSect = `\n## Usage \n${usage} \n`;
-    const licenseSect = `\n## License \n${license} \n`;
-    const contributeSect = `\n## Contributing \n${contributing} \n`;
-    const testsSect = `\n## Tests \n${tests} \n`;
-    const questionsSect = "\n## Questions \n" + "* Username: " + github + "\n" + "* Email: " + email;
-
-    return installSect + useSect + licenseSect + contributeSect + testsSect + questionsSect;
-
+    const sections = [
+        "## Installation \n" + installation,
+        "## Usage \n" + usage,
+        "## License \n" + license,
+        "## Contributing \n" + contributing,
+        "## Tests \n" + tests,
+        "## Questions \n" + "* Username: " + github + "\n*Email: " + email
+    ].join("\n\n");
+    return sections;
 }
-
